cb8604736b6b1b927386b907812b939e9f696806
Justin <52802645+mahogany-dev@users.noreply.github.com>
2021-03-28 18:47:07 +0200

    Implemented DB CRUD operations

--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
 
+const sqlite = require('sqlite3').verbose();
 const { Client } = require('discord.js');
 const fs = require('fs'); 
 const { get } = require('http');
+const path = require('path');
 const client = new Client();
-const PREFIX = "~";
-const filename = "Output.txt";
+const PREFIX = '~';
+const filename = 'Output.txt';
 
 client.on('ready', () => {
+    // validate login
     console.log(`${client.user.username} has logged in.`);
-    client.user.setActivity("Among Us", {
-        type: "PLAYING"
+    // set activity
+    client.user.setActivity('Among Us', {
+        type: 'PLAYING'
     });
+    // prepare sqlite db
+    let db = new sqlite.Database('./testdb.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
 });
 
 client.on('message', (message) => {
@@ -28,24 +34,26 @@ client.on('message', (message) => {
      // set cmd & args   
      const [CMD_NAME, ...args] = getCMD(message);
      // check cmds
-     switchCMD(message, CMD_NAME, args)
+     // prep db
+     let db = new sqlite.Database('./testdb.db', sqlite.OPEN_READWRITE);
+     switchCMD(message, CMD_NAME, args, db);
     }    
 });
 
-function switchCMD(message, CMD_NAME, args) {
+function switchCMD(message, CMD_NAME, args, db) {
     switch (CMD_NAME) {
         case 'add':
-            writeIntoFile(args);
+            writeIntoFile(args, message, db);
             break;
         case 'read':
-            outputFile(message);
+            outputFile(message, db);
             break;
         case 'clear':
-            clearFile(message);
+            clearDB(message, db);
             break;  
-        case 'test':
-            test(message);
-            break;
+        case 'change':
+            changeWord(message, db, args);
+            break;    
         default:
             // invalid command
             break;
@@ -53,57 +61,85 @@ function switchCMD(message, CMD_NAME, args) {
 }
 
 function getCMD(message) {
+    //
+    // prep msg
+    //
     return message.content.trim()
     .substring(PREFIX.length)
     .split(" ");
 }
 
-function writeIntoFile(args) {
-    fs.writeFile(filename, args.join(', '), { flag: "a" }, function(err) {
-        if (err) {
-          console.log("file " + filename + " already exists");
-        }
-        else {
-          console.log("Succesfully written " + filename);
-        }
+function writeIntoFile(args, message, db) {
+      //
+      // garner user infos & prepare db
+      //
+      let userid = message.author.id;
+      // db.run(`CREATE TABLE IF NOT EXISTS data(userid INTEGER NOT NULL, keyword TEXT NOT NULL, audio BLOB)`);
+
+      //
+      // insert data into db
+      //
+      let insertdata = db.prepare('INSERT INTO data VALUES(?,?,?)');
+      insertdata.run(userid, args, ' ');
+      insertdata.finalize();
+      db.close();
+
+      // validate
+
+      console.log('wrote into db!');
+}
+
+function outputFile(message, db) {
+
+    //
+    // WIP 
+    //
+    // let db = new sqlite.Database('./testdb.db', sqlite.OPEN_READWRITE);
+
+    let query = `SELECT keyword FROM data`; 
+      db.each(query, (err, row) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (row === undefined) {
+                console.log('undefined row');
+            }
+
+            let keyword = row.keyword;
+            message.channel.send(keyword);
       });
+
+      // validate
+      console.log('successfully read from db!');
 }
 
-function outputFile(message) {
-    // return if file is empty
-    console.log('reading file');
-
-    fs.readFile(filename, (err, data) => { 
-        if (err) throw err; 
-        
-        var fileData = data.toString();
-        message.channel.send(fileData);
-    });
+function clearDB(message, db) {
+    //
+    // TODO
+    //
+    // message.channel.send("DB has been cleared!");
 }
 
-function clearFile(message) {
-    fs.truncate(filename, 0, function(){console.log('cleared file!')})
-    message.channel.send("File has been cleared!");
+function changeWord(message, db, args) {
+
+    //
+    // validate & set input 
+    //
+    if (args.length != 2) {
+        message.reply('Invalid usage! Please use following syntax: ~change <old word> <new word>');
+        return;
+    }
+    let oldWord = args[0];
+    let newWord = args[1];
+
+    db.run(`UPDATE data SET keyword = ? WHERE keyword = ?`, [newWord, oldWord]);
+
+    message.reply(`Replaced ${oldWord} with ${newWord}!`);
+    console.log('replaced word!');
 }
 
-function deleteMSG() {
-
-    fs.readFile(filename, (err, data) => { 
-        if (err) throw err; 
-        
-        var fileData = data.toString().split(",");
-        processData(fileData);
-    });
-}
-
-function processData(content) {
-    console.log(content[1]);
-}
-
-// test environment for functions
-function test() {
-    deleteMSG();
-}
-
-
+//
+// client login
+//
 client.login(process.env.DISCORDJS_BOT_TOKEN);
